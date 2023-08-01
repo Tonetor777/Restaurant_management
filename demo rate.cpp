8d5afcac05dc0d97e04497881cc34bd10ce0feae
salemhabte <habtesalem20@gmail.com>
2023-08-01 12:02:47 -0700

    added file

--- demo rate.cpp
+++ demo rate.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include<fstream>
 #include <string>
 using namespace std;
 double average;
+ofstream outfile1;
 void rating(int count){
 	int x,ave;
 	double sum=0;
-	char comment[50];
+    string comment;
 	for(int i=0;i<count;i++){
 	label:
 	cout<< "Rate the restaurant ordering system from 1-5 please: ";
@@ -29,6 +31,8 @@ void rating(int count){
 	 sum+=ave;
 	 average= sum/count;
 	 cout<<endl;
+	 
+	outfile1.open("my rate.txt", ios::app);
 	
 	 char yn;
 	 cout<<"would you leave your comment about our food (y/n): ";
@@ -37,15 +41,18 @@ void rating(int count){
 	 cout<<endl; 
 	 if(yn=='y'){
 	 cout<<"comment: ";
-	 gets(comment);
-	 
+	 getline(cin, comment);
+	  outfile1<<"comment:"<<comment<<endl;
 	 }
 	 else
-	 cout<<"Thank you for choosing us, come again!!!"<<endl;}
+	 cout<<"Thank you for choosing us, come again!!!"<<endl;
+	 outfile1.close();}
 }
 void averate(double average,int count){
 	cout<<"average daily rating is: "<<average<<" out of "<<count<<" customers"<<endl;
-	
+	outfile1.open("my rate.txt", ios::app);
+	outfile1<<"average daily rating is: "<<average<<" out of "<<count<<" customers"<<endl;
+	outfile1.close();
 }
 int main(){
 	int c=2;
